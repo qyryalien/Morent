f1c67fbda8ddd5e8a9b6f5b41739eb7e8b4df5cf
Qyry <qyryalien@gmail.com>
2023-04-03 06:10:05 +0300

    Added paths for display by category, registration and authorization.

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', main, name='home')
+    path('', CarHome.as_view(), name='home'),
+    path('category/<slug:cat_slug>/', CarCategory.as_view(), name='category'),
+    path('steering/<slug:steering_slug>/', CarSteering.as_view(), name='steering'),
+    path('capacity/<slug:capacity_slug>/', CarCapacity.as_view(), name='capacity'),
+    path('register/', RegisterUser.as_view(), name='register'),
+    path('login/', LoginUser.as_view(), name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
