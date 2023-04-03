3d167e6db2084ad51c8d72c4d06373b4aad37ea8
Qyry <qyryalien@gmail.com>
2023-04-03 06:11:59 +0300

    Added context with categories.

--- core/utils.py
+++ core/utils.py
@@ -2,6 +2,18 @@ from django.db.models import Count
 from .models import *
 
 
+
+
+
 class DataMixin:
     def get_user_context(self, **kwargs):
-        pass
+        context = kwargs
+        cats = Category.objects.all()
+        steer = Steering.objects.all()
+        capas = Capacity.objects.all()
+        context['cats'] = cats
+        context['steer'] = steer
+        context['capas'] = capas
+        if 'cat_selected' not in context:
+            context['cat_selected'] = 0
+        return context
