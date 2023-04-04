961e61db914c4d4bddf2fc75035fd0cdc4a6b311
Qyry <qyryalien@gmail.com>
2023-04-04 23:14:35 +0300

    Added path for auto page

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('category/<slug:cat_slug>/', CarCategory.as_view(), name='category'),
     path('steering/<slug:steering_slug>/', CarSteering.as_view(), name='steering'),
     path('capacity/<slug:capacity_slug>/', CarCapacity.as_view(), name='capacity'),
+    path('post/<slug:car_slug>/', ShowCar.as_view(), name='car'),
     path('register/', RegisterUser.as_view(), name='register'),
     path('login/', LoginUser.as_view(), name='login'),
     path('logout/', logout_user, name='logout'),
