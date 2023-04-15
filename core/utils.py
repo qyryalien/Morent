c1b8e112b675f048e2f3c904981d8ebf5a4b7aa6
Qyry <qyryalien@gmail.com>
2023-04-15 09:33:21 +0300

    Added pagination for main and categories pages.

--- core/utils.py
+++ core/utils.py
@@ -1,11 +1,12 @@
 from django.db.models import Count
+from django.shortcuts import render
+
 from .models import *
 
 
-
-
-
 class DataMixin:
+    paginate_by = 6
+
     def get_user_context(self, **kwargs):
         context = kwargs
         cats = Category.objects.all()
