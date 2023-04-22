466d268d0890ea4588644984158f5120daae91da
Qyry <qyryalien@gmail.com>
2023-04-22 15:29:09 +0300

    Added view function for change password page.

--- core/views.py
+++ core/views.py
@@ -1,10 +1,17 @@
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import SetPasswordForm, PasswordResetForm
+from django.contrib.auth.tokens import default_token_generator
 from django.contrib.auth.views import LoginView
-from django.http import HttpResponseNotFound, request
+from django.core.mail import send_mail, BadHeaderError
+from django.db.models import Q
+from django.http import HttpResponseNotFound, request, HttpResponse
 from django.shortcuts import redirect
+from django.template.loader import render_to_string
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 from django.views import View
 from django.views.generic import ListView, CreateView, DetailView
 from django.contrib import messages
@@ -238,3 +245,33 @@ class ChangeUserData(View):
             return redirect(link)
         context = {'form': form, 'title': 'Edit profile', 'pk': self.kwargs['pk']}
         return render(self.request, 'core/change_user_data.html', context)
+
+
+def password_reset_request(request):
+    if request.method == "POST":
+        password_reset_form = PasswordResetForm(request.POST)
+        if password_reset_form.is_valid():
+            data = password_reset_form.cleaned_data['email']
+            associated_users = User.objects.filter(Q(email=data))
+            if associated_users.exists():
+                for user in associated_users:
+                    subject = "Password Reset Requested"
+                    email_template_name = "core/password/password_reset_email.txt"
+                    c = {
+                        "email": user.email,
+                        'domain': '127.0.0.1:8000',
+                        'site_name': 'Morent',
+                        "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                        "user": user,
+                        'token': default_token_generator.make_token(user),
+                        'protocol': 'http',
+                    }
+                    email = render_to_string(email_template_name, c)
+                    try:
+                        send_mail(subject, email, 'qyryalien@gmail.com', [user.email], fail_silently=False)
+                    except BadHeaderError:
+                        return HttpResponse('Invalid header found.')
+                    return redirect("/profile/password_reset/done/")
+    password_reset_form = PasswordResetForm()
+    return render(request=request, template_name="core/password/password_reset.html",
+                  context={"password_reset_form": password_reset_form})
