f60715bd650ebd30e0af38aa4089a2694b43cc6d
Qyry <qyryalien@gmail.com>
2023-04-22 15:27:27 +0300

    Added paths for password reset pages.

--- Morent/urls.py
+++ Morent/urls.py
@@ -18,18 +18,28 @@ from django.contrib import admin
 from django.urls import path, include
 from core.views import *
 from Morent import settings
-
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls'))
+    path('', include('core.urls')),
+    path('profile/password_reset/done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='core/password/password_reset_done.html'),
+         name='password_reset_done'),
+    path('profile/password_reset/reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name="core/password/password_reset_confirm.html"),
+         name='password_reset_confirm'),
+    path('profile/password_reset/reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='core/password/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
+
     urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
+                      path('__debug__/', include(debug_toolbar.urls)),
+                  ] + urlpatterns
 
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
