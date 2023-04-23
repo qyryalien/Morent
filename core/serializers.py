efbe240edfb417e71f1647a4bdfae067cd1babbe
Qyry <qyryalien@gmail.com>
2023-04-23 21:47:59 +0300

    Updated UserSerializer. Added ChangePasswordSerializer for password change.

--- core/serializers.py
+++ core/serializers.py
@@ -4,13 +4,27 @@ from django.contrib.auth.models import User
 
 # User Serializer
 class UserSerializer(serializers.ModelSerializer):
+    """
+        Serializer for user
+    """
+
     class Meta:
         model = User
-        fields = ('id', 'username', 'email')
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'date_joined')
+
+    def update(self, instance, validated_data):
+        for(key, value) in validated_data.items():
+            setattr(instance, key, value)
+        instance.save()
+        return instance
 
 
 # Register Serializer
 class RegisterSerializer(serializers.ModelSerializer):
+    """
+    Serializer for user registration
+    """
+
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'password')
@@ -19,3 +33,13 @@ class RegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
         return user
+
+
+class ChangePasswordSerializer(serializers.Serializer):
+    """
+    Serializer for password change endpoint.
+    """
+    model = User
+
+    new_password = serializers.CharField(required=True)
+    new_password_confirm = serializers.CharField(required=True)
