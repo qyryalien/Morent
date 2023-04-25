ac984d1e0a1c505c476045a665c8f812c4ad254d
Qyry <qyryalien@gmail.com>
2023-04-25 09:23:59 +0300

    Remove path without API. Added path with API.

--- core/urls.py
+++ core/urls.py
@@ -4,27 +4,20 @@ from . import views
 from .views import *
 
 urlpatterns = [
-    path('', CarHome.as_view(), name='home'),
-    path('category/<slug:cat_slug>/', CarCategory.as_view(), name='category'),
-    path('steering/<slug:steering_slug>/', CarSteering.as_view(), name='steering'),
-    path('capacity/<slug:capacity_slug>/', CarCapacity.as_view(), name='capacity'),
-    path('post/<slug:car_slug>/', ShowCar.as_view(), name='car'),
-    path('register/', RegisterUser.as_view(), name='register'),
-    path('login/', LoginUser.as_view(), name='login1'),
-    path('logout/', logout_user, name='logout'),
-    path('profile/<slug:pk>/', Profile.as_view(), name='profile'),
-    path('payment/<car>/', Payment.as_view(), name='payment'),
-    path('profile/<slug:pk>/edit', ChangeUserData.as_view(), name='profile_edit'),
-    path('profile/password_reset', views.password_reset_request, name='password_reset'),
     # API
     path('api/register/', RegisterAPI.as_view(), name='register'),
     path('api/login/', LoginAPI.as_view(), name='login'),
     path('api/logout/', knox_views.LogoutView.as_view(), name='logout'),
     path('api/logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
-    path('api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
+    path('api/password_reset/<pk>', ChangePasswordView.as_view(), name='password_reset'),
     path('api/send_password_reset_email/', SendChangePasswordEmailView,
          name='send_password_resel_email'),
-    path('api/check_password_reset_token/', CheckChangePasswordTokenView.as_view(),
+    path('api/check_password_reset_token/<uidb64>/<token>', CheckChangePasswordTokenView.as_view(),
          name='check_password_reset_token'),
     path('api/profile/<pk>', UserRetrieveUpdateAPIView.as_view(), name='profile'),
+    path('api/', CarListAPIView.as_view(), name='home'),
+    path('api/car/<pk>', CarRetrieveAPIView.as_view(), name='home'),
+    path('api/filter/', CarFilterListAPIView.as_view(), name='filter'),
+    path('api/profile/<pk>/orders', OrderListAPIView.as_view(), name='orders'),
+    path('api/payment/', OrderCreateAPIView.as_view(), name='payment')
 ]
