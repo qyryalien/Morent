e0987000590dabf0fceb4c6a7a78e896b327fe9f
Qyry <qyryalien@gmail.com>
2023-05-06 13:15:42 +0300

    Added ReviewSerializer for Review model.

--- core/serializers.py
+++ core/serializers.py
@@ -91,3 +91,9 @@ class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = "__all__"
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = "__all__"
