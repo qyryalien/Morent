ed1267f444e1495790a124aefc23f2691f8f3479
Qyry <qyryalien@gmail.com>
2023-05-06 13:14:11 +0300

    Added DATABASES settings for switching between the local database and the database from docker.

--- Morent/settings.py
+++ Morent/settings.py
@@ -90,13 +90,23 @@ WSGI_APPLICATION = 'Morent.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('POSTGRES_NAME'),
+#         'USER': os.environ.get('POSTGRES_USER'),
+#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+#         'HOST': 'db',
+#         'PORT': 5432,
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_NAME'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'db',
+        'NAME': 'Morent',
+        'USER': 'postgres',
+        'PASSWORD': 'qyry1308',
         'PORT': 5432,
     }
 }
