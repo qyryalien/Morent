8dc2f581e8c4475db24d6f5ef8323f0d45add8fd
Dmytro Hmelnitskiy <127364930+qyryalien@users.noreply.github.com>
2023-06-09 15:25:55 +0300

    Update settings.py
    Delete comment

--- Morent/settings.py
+++ Morent/settings.py
@@ -183,16 +183,7 @@ SIMPLE_JWT = {
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
 }
-#
-# REST_KNOX = {
-#     'SECURE_HASH_ALGORITHM':'cryptography.hazmat.primitives.hashes.SHA512',
-#     'AUTH_TOKEN_CHARACTER_LENGTH': 64, # By default, it is set to 64 characters (this shouldn't need changing).
-#     'TOKEN_TTL': timedelta(minutes=3), # The default is 10 hours i.e., timedelta(hours=10)).
-#     'USER_SERIALIZER': 'knox.serializers.UserSerializer',
-#     'TOKEN_LIMIT_PER_USER': None, # By default, this option is disabled and set to None -- thus no limit.
-#     'AUTO_REFRESH': False, # This defines if the token expiry time is extended by TOKEN_TTL each time the token is used.
-#     'EXPIRY_DATETIME_FORMAT': api_settings.DATETIME_FORMAT,
-# }
+
 
 
 CORS_ORIGIN_ALLOW_ALL = True
