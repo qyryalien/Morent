a00fd83e7ed90ac1faca3eabd5a6aef97ab74279
Dmytro Hmelnitskiy <127364930+qyryalien@users.noreply.github.com>
2023-06-22 14:33:11 +0300

    Update axiosBaseSettings.js

--- FrontEnd/src/axiosConfigs/axiosBaseSettings.js
+++ FrontEnd/src/axiosConfigs/axiosBaseSettings.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-	baseURL: "http://127.0.0.1:8000",
+	baseURL: "https://morent-kv7s.onrender.com:80",
 });
 
 instance.interceptors.request.use((config) => {
