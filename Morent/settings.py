cfd1444437435ae4d09ddc11a7987cde58f6b836
Dmytro Hmelnitskiy <127364930+qyryalien@users.noreply.github.com>
2023-06-22 16:06:27 +0300

    Update settings.py

--- Morent/settings.py
+++ Morent/settings.py
@@ -19,7 +19,8 @@ from core.keys import ACCESS_KEY_ID, SECRET_ACCESS_KEY
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+BACKEND_DIR = BASE_DIR
+FRONTEND_DIR = BASE_DIR / 'FrontEnd'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
