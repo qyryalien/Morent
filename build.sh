bd50a9a6e2b5e7a628d78e4baeacdb092b42c7b1
Dmytro Hmelnitskiy <127364930+qyryalien@users.noreply.github.com>
2023-06-22 16:26:02 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,14 @@
 # exit on error
 set -o errexit
 
+cd FrontEnd
+mkdir -p build/root
+for file in $(ls build | grep -E -v '^(index\.html|static|root)$'); do
+    mv "build/$file" build/root;
+done
+
+cd .
+
 pip install --upgrade pip; pip install poetry; /opt/render/project/src/.venv/bin/poetry install
 python manage.py collectstatic --no-input
 python manage.py migrate
