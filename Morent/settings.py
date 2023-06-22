1df373529d437dd897c74aaa2d3f921c29dc6ab3
Dmytro Hmelnitskiy <127364930+qyryalien@users.noreply.github.com>
2023-06-22 14:23:55 +0300

    Update settings.py

--- Morent/settings.py
+++ Morent/settings.py
@@ -152,7 +152,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'FrontEnd/public/static/'
 
 # Following settings only make sense on production and may break development environments.
 if not DEBUG:
