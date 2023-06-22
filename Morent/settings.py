05aa2857a04d5b4d4e8ae8d6c3c101d3b302b739
Dmytro Hmelnitskiy <127364930+qyryalien@users.noreply.github.com>
2023-06-22 09:59:35 +0300

    Update settings.py

--- Morent/settings.py
+++ Morent/settings.py
@@ -103,7 +103,7 @@ WSGI_APPLICATION = 'Morent.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='postgresql://postgres:postgres@localhost:5432/morent',
         conn_max_age=600
     )
 }
