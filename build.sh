dc4917b81f84aaefe320eab12599ff8110e7e8d5
Dmytro Hmelnitskiy <127364930+qyryalien@users.noreply.github.com>
2023-06-22 19:20:29 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -o errexit
 
 pip install --upgrade pip; pip install poetry; /opt/render/project/src/.venv/bin/poetry install
 python manage.py collectstatic --clear
-python manage.py findstatic 1.png
+python manage.py findstatic 4.png
 python manage.py makemigrations core
 python manage.py migrate
