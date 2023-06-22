ea58e2ac290207bfa883589bc9c03b4e4e03d08b
Dmytro Hmelnitskiy <127364930+qyryalien@users.noreply.github.com>
2023-06-22 16:35:58 +0300

    Update urls.py

--- Morent/urls.py
+++ Morent/urls.py
@@ -23,7 +23,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    re_path(r'', core.views.catchall)
+    re_path(r'', catchall)
 ]
 
 if settings.DEBUG:
