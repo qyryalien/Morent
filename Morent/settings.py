a0dfb055458fe38aaab0767927d3933b2989ff0e
Dmytro Hmelnitskiy <127364930+qyryalien@users.noreply.github.com>
2023-06-24 07:00:29 +0300

    Update settings.py

--- Morent/settings.py
+++ Morent/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 # Here, they well be accessible at your-domain.onrender.com/static/...
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'staticfiles']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
