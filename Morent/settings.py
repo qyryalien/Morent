28650e6427b14579e7460640b518296805be69a7
Dmytro Hmelnitskiy <127364930+qyryalien@users.noreply.github.com>
2023-06-24 08:10:34 +0300

    Update settings.py

--- Morent/settings.py
+++ Morent/settings.py
@@ -147,9 +147,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-MEDIA_URL = 'static/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
