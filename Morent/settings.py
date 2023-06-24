94dd5d55cba1f18787a6246f0d1113d1bef3ba00
Dmytro Hmelnitskiy <127364930+qyryalien@users.noreply.github.com>
2023-06-24 06:16:04 +0300

    Update settings.py

--- Morent/settings.py
+++ Morent/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar',
     'core',
     'rest_framework',
     'django_rest_passwordreset',
@@ -62,7 +61,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'corsheaders.middleware.CorsMiddleware',
 ]
 
