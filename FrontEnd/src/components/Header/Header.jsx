f95f8c7be9dc3eaefc2739bb1443848e45887de8
NicholasVB <tivalik2@gmail.com>
2023-07-26 15:04:50 +0200

    create burger menu

--- FrontEnd/src/components/Header/Header.jsx
+++ FrontEnd/src/components/Header/Header.jsx
@@ -10,7 +10,38 @@ export const Header = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
     let isAuth = useSelector(state => state.auth.curentAuthSession);
-   
+    
+    
+    let [size, setSize] = React.useState({});
+    const ref = React.useRef(window);
+    const baseSizeList = [767, 1440];
+    
+    const resizeHandler = () => {
+        let { innerHeight, innerWidth } = ref.current || {};
+        for (let baseSize of baseSizeList) {
+            if (baseSize >= innerWidth) {
+                innerWidth = baseSize
+                break;
+            }
+        }
+        if (innerWidth > baseSizeList[baseSizeList.length - 1]) {
+            innerWidth = baseSizeList[baseSizeList.length - 1];
+        }
+        setSize({ innerHeight, innerWidth });
+    };
+
+
+    React.useEffect(() => {
+
+        window.addEventListener("resize", resizeHandler);
+        resizeHandler();
+
+        return () => {
+            window.removeEventListener("resize", resizeHandler);
+        };
+    }, [])
+
+    let HTMLsizeChoiser = new Map();
 
     async function logout() {
         let refresh = window.localStorage.getItem("refresh")
@@ -20,8 +51,8 @@ export const Header = () => {
         dispatch(setCurentAuthSession(false));
         navigate("/");
     }
-    
-    return(
+
+    HTMLsizeChoiser.set(1440,
         <>
             <header className="header">
                 <div className="header__container">
@@ -32,9 +63,9 @@ export const Header = () => {
                     </div>
                     <div className="header__menu menu">
                         <div className="menu__items">
-                            <div className="menu__item"><div to="/About" className="menu__item-link">About</div></div>
+                            <Link className="menu__item"><div to="/About" className="menu__item-link">About</div></Link>
                             {/* <div className="menu__item"><div to="/Community" className="menu__item-link">Community</div></div> */}
-                            <div className="menu__item"><div to="/socials" className="menu__item-link">Socials</div></div>
+                            <Link className="menu__item"><div to="/socials" className="menu__item-link">Socials</div></Link>
                         </div>
                     </div>
                     <div className="header__btns-block">
@@ -55,5 +86,58 @@ export const Header = () => {
                 
             </header>
         </>
+    )
+    
+    function toggleBurgerState (e) { 
+        document.querySelector(".icon-menu").classList.toggle("_active");
+        document.querySelector(".menu").classList.toggle("open");
+        document.querySelector(".menu__items").classList.toggle("open");
+    }
+
+    HTMLsizeChoiser.set(767,
+        <>
+            <header className="header">
+                <div className="header__container">
+                    <div className="logo" onClick={() => {
+                        if (document.querySelector(".icon-menu").classList.contains("_active")) {
+                            toggleBurgerState()
+                        }
+                        }}>
+                        <div className="logo__text">
+                            <Link to="/">MORENT</Link>
+                        </div>
+                    </div>
+                    
+                    <div className="header__btns-block">
+                        <button type='button' class='icon-menu' onClick={toggleBurgerState}>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </button>
+                    </div>
+                </div>
+                
+            </header>
+            <div className="header__menu menu">
+                <div className="menu__items" onClick={toggleBurgerState}>
+                    <Link className="menu__item" to="/About"><div  className="menu__item-link">About</div></Link>
+                    <Link className="menu__item" to="/socials"><div className="menu__item-link">Socials</div></Link>
+                    {isAuth 
+                        ? <Link to="/profile" className="menu__item"><div className="menu__item-link">Profile</div></Link>
+                        : <Link to="/login" className="menu__item menu__item-link"><div className="menu__item-link">Login</div></Link> 
+                    }
+                    {isAuth 
+                        ? <button className="menu__item" onClick={logout}>                        
+                            <div className="menu__item-link">Logout</div>
+                            </button>
+                        : <div></div> 
+                    }
+                </div>
+            </div>
+        </>    
+    )
+    
+    return(
+        HTMLsizeChoiser.get(size.innerWidth)
     )
 }
