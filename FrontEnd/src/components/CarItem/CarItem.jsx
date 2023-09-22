42d78a72e79b4c386945a046f23d64ca1eb248cf
NicholasVB <tivalik2@gmail.com>
2023-09-22 07:53:14 +0200

    Fix photo size and fill, make it same in CarSCSS. Fix images links in carItemJSX

--- FrontEnd/src/components/CarItem/CarItem.jsx
+++ FrontEnd/src/components/CarItem/CarItem.jsx
@@ -48,7 +48,8 @@ export const CarItem = ({id, carName, carClass, imgUrl, specs, price}) => {
                 <div className="item-body__carClass">{carClass}</div>
             </div>
             <div className="item-body__img">
-                <img src={`https://morent-backend-xavm.onrender.com/static${imgUrl}`} alt=""/>
+                {/* <img src={`https://morent-backend-xavm.onrender.com/static${imgUrl}`} alt=""/> */}
+                <img src={imgUrl} alt=""/>
             </div>
             <div className="item-body__specs">
                 {specs 
@@ -81,7 +82,8 @@ export const CarItem = ({id, carName, carClass, imgUrl, specs, price}) => {
                 <div className="item-body__carClass">{carClass}</div>
             </div>
             <div className="item-body__img">
-                <img src={`https://morent-backend-xavm.onrender.com/static${imgUrl}`} alt=""/>
+                {/* <img src={`https://morent-backend-xavm.onrender.com/static${imgUrl}`} alt=""/> */}
+                <img src={imgUrl} alt=""/>
             </div>
             <div className="item-body__specs specs">
                 {specs 
@@ -116,7 +118,8 @@ export const CarItem = ({id, carName, carClass, imgUrl, specs, price}) => {
                 </div>
                 <div className='item-body__center-block'>
                     <div className="item-body__img">
-                        <img src={`https://morent-backend-xavm.onrender.com/static${imgUrl}`} alt=""/>
+                        {/* <img src={`https://morent-backend-xavm.onrender.com/static${imgUrl}`} alt=""/> */}
+                        <img src={imgUrl} alt=""/>
                     </div>
                     <div className="item-body__specs">
                     {specs 
